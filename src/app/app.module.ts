337274277298d2104f18aeedc99ad317e09c4389
thabomakwea <thabomakwea@gmail.com>
2022-05-23 20:40:12 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from '@angular/common';
+import { CarouselComponent } from './components/carousel/carousel.component';
 import { AppComponent } from './app.component';
+import { SwiperModule } from "swiper/angular";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, CommonModule, FormsModule, SwiperModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,79 @@
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import data from './data.json';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./styles/carousel.component.scss']
+})
+export class CarouselComponent implements OnInit {
+
+  public carousel = data;
+  public settings = {
+    container: null,
+    cards: [],
+    card: null,
+    addCard: (cardsContainer: any, card: any, direction: any) => {
+      cardsContainer.insertAdjacentHTML('beforeend', card.outerHTML)
+    },
+    moveCards: (container: any, cards: any[], card: any, direction: any) => { 
+      switch (direction) {
+        case 'left':
+          cards[0].className = "col-2 box move-out-from-left";
+          setTimeout(function () {
+            [1,2,3,4].forEach(cardNumber => {
+              cards[cardNumber].className = (cardNumber === 3 ? 'col-4 ' : 'col-2 ' ) + "box move-to-position" + String(cardNumber) + "-from-" + direction;
+            });
+            cards[0].remove()
+          }, 5);
+          break;
+        case 'right':
+          cards[4].className = "col-2 box move-out-from-" + direction;
+          setTimeout(function() {
+              card.classList.remove("box--hide");
+              cards[cards.length - 1].remove();
+              container.insertBefore(card, container.firstChild);
+              card.className = "col-2 box move-to-position1-from-" + direction;
+              [0, 1, 2, 3].forEach(cardNumber => {
+                cards[cardNumber].className = (cardNumber === 1 ? 'col-4 ' : 'col-2 ') + "box move-to-position" + String(cardNumber) + "-from-" + direction;
+              });
+          }, 5);
+          break;
+        default:
+          break;
+      }
+    },
+  }
+  constructor(
+    private _el: ElementRef,
+  ) { }
+
+  ngOnInit() {
+  }
+
+  useState(state: any) {
+    this.settings.container = this._el.nativeElement.querySelector('.carousel .row')
+    this.settings.cards = this._el.nativeElement.querySelectorAll('.carousel .row > div') 
+    switch (state) {
+      case 'previous':
+          this.settings.card = this._el.nativeElement.querySelectorAll('.carousel .row > div')[0]
+        break;
+      case 'next':
+        this.settings.card = this._el.nativeElement.querySelectorAll('.carousel .row > div')[this.settings.cards.length - 1]
+        break;
+      default:
+        break;
+    }
+  }
+
+  previousCard() {
+    this.useState('previous')
+    this.settings.moveCards(this.settings.container, this.settings.cards || [], this.settings.card, 'left')
+    this.settings.addCard(this.settings.container, this.settings.card, 'left' ) 
+  }
+
+  nextCard() {
+    this.useState('next')
+    this.settings.moveCards(this.settings.container, this.settings.cards || [], this.settings.card, 'right')
+  }
+}
